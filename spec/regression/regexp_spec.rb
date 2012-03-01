02f3520428573c57c4049feab8af96d0aed2ae02
Michael Jackson <mjijackson@gmail.com>
2012-02-29 16:21:23 -0800

    Use File.expand_path for 1.9's sake

--- spec/regression/regexp_spec.rb
+++ spec/regression/regexp_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.expand_path('../../spec_helper', __FILE__)
 
 describe "#{'*' * 80}\nRegression Specs:" do
   it "/abcd/                          => 'abcd'" do
